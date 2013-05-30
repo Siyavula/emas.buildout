86eb720db7e9a8ae120cb684b358bceeb27ddbe7
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-05-30 16:34:02 +0200

    Renamed

---
+++ scripts/create_memberservices_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE MEMBERSERVICES (
+    MEMBERSERVICE_ID serial primary key,
+    USERID varchar(100) not null,
+    RELATED_SERVICE integer not null,
+    EXPIRY_DATE date,
+    CREDITS integer,
+    SERVICE_TYPE varchar(100),
+    ZOPE_UID integer unique
+);
