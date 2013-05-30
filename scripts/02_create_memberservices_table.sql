298ffaef781158914e06a9a0c9e9157a666d8568
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-05-30 16:32:59 +0200

    Renamed since I want to keep the sql and the shell bits separate.

--- scripts/02_create_memberservices_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE MEMBERSERVICES (
-    MEMBERSERVICE_ID serial primary key,
-    USERID varchar(100) not null,
-    RELATED_SERVICE integer not null,
-    EXPIRY_DATE date,
-    CREDITS integer,
-    SERVICE_TYPE varchar(100)
-);
