fd0533a61e93922522e13d4292dfb88fd30e0d8a
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-06-07 15:20:47 +0200

    Rename column

--- scripts/create_memberservices_table.sql
+++ scripts/create_memberservices_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE MEMBERSERVICES (
     MEMBERSERVICE_ID serial primary key,
     USERID varchar(100) not null,
-    RELATED_SERVICE integer not null,
+    TITLE varchar(100) not null,
+    RELATED_SERVICE_ID integer not null,
     EXPIRY_DATE date,
     CREDITS integer,
     SERVICE_TYPE varchar(100),
