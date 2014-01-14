477c9db034960ca7a28e6e15c93c1fbac005b0b2
Roché Compaan <roche@upfrontsystems.co.za>
2014-01-14 16:23:31 +0200

    don't clear BTree, we want to skip orders that have already been added

--- scripts/add_orders_to_verificationcode_utility.py
+++ scripts/add_orders_to_verificationcode_utility.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 import transaction
+from sets import Set
 
 from Testing import makerequest
 from AccessControl.SecurityManagement import newSecurityManager
@@ -16,20 +17,27 @@ from logging import getLogger
 
 TIME_FORMAT = '%H:%M:%S:%s'
 
+verification_codes = Set()
 
 def process(portal):
     print '-------------------------------------------------------------------'
     print('Started at:%s' % datetime.datetime.now().strftime(TIME_FORMAT))
     vcu = getUtility(IVerificationCodeUtility, context=portal)
-    vcu._verification_codes.clear()
     for count, order in enumerate(portal.orders.objectValues()):
         try:
             if order.verification_code:
                 vc = int(order.verification_code)
+                if vc in verification_codes:
+                    print 'Duplicate verification code:%s for order:%s' % (
+                        vc, order.getId())
+                    continue
+
                 if vcu.is_unique(vc):
                     vcu.add(vc, order)
-                else:
-                    print 'Duplicate verification code:%s for order:%s' % (vc, order.getId())
+
+                verification_codes.add(vc) 
+
+
         except ConflictError as e:
             print 'Could not add order:%s' % order.getId()
         if not count % 1000:
