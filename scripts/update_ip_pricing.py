0b74d396c7fcd655f28d25eb5848321a3346e170
Izak Burger <isburger@gmail.com>
2015-01-09 17:22:46 +0200

    Update price update script.

--- scripts/update_ip_pricing.py
+++ scripts/update_ip_pricing.py
@@ -35,6 +35,8 @@ newSecurityManager(None, user.__of__(app.acl_users))
 pmt = getToolByName(portal, 'portal_membership')
 
 service_ids = [
+    'maths-grade8-practice',
+    'maths-grade9-practice',
     'maths-grade10-practice',
     'maths-grade11-practice',
     'maths-grade12-practice',
@@ -48,12 +50,14 @@ products_and_services = portal['products_and_services']
 for sid in service_ids:
     service = products_and_services[sid]
     service.subscription_period = 365
-    # the total price for the 3 grades should amount to R350, this is why
+    # the total price for the 3 grades should amount to R400, this is why
     # we have such odd prices
-    if sid in ('maths-grade10-practice', 'science-grade10-practice'):
-        service.price = 116.66
+    if sid.startswith('maths'):
+        service.price = 80 # 5 services, R400
+    elif sid in ('science-grade12-practice',):
+        service.price = 134 # 3 services, total must be R400
     else:
-        service.price = 116.67
+        service.price = 133
     print "Updating pricing and subscription period for ", service.Title()
 
 transaction.commit()
