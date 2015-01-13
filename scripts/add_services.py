b680b07a6dae56fd4bcd484ba99a0618562a8f1c
Izak Burger <isburger@gmail.com>
2015-01-13 09:24:30 +0200

    Refactor script slightly, also add the monthly practice services.

--- scripts/add_services.py
+++ scripts/add_services.py
@@ -22,24 +22,36 @@ wf = getToolByName(portal, 'portal_workflow')
 
 products_and_services = portal['products_and_services']
 
+def add_service(id, **kw):
+    if not products_and_services.hasObject(id):
+        print "Adding service " + id
+        products_and_services.invokeFactory(id=id, type_name='emas.app.service',
+            service_type='subscription', amount_of_credits=0, **kw)
+        ob = products_and_services[id]
+        wf.doActionFor(ob, 'publish')
+        # Reindex, probably to get the Title right if memory serves
+        ob.reindexObject()
+        return ob
+    return None
+
 # Create services
-products_and_services.invokeFactory(type_name='emas.app.service',
-    id='maths-grade8-practice', price=80.0, grade=u'grade-8',
-    service_type='subscription', subscription_period=365,
-    amount_of_credits=0, access_path='@@practice/grade-8',
-    subject=u'maths', title=u'Maths Grade 8 Intelligent Practice')
-products_and_services.invokeFactory(type_name='emas.app.service',
-    id='maths-grade9-practice', price=80.0, grade=u'grade-9',
-    service_type='subscription', subscription_period=365,
-    amount_of_credits=0, access_path='@@practice/grade-9',
-    subject=u'maths', title=u'Maths Grade 9 Intelligent Practice')
+add_service('maths-grade8-practice',
+    price=80.0, grade=u'grade-8', subscription_period=365,
+    access_path='@@practice/grade-8', subject=u'maths',
+    title=u'Maths Grade 8 Intelligent Practice')
+add_service('maths-grade9-practice',
+    price=80.0, grade=u'grade-9', subscription_period=365,
+    access_path='@@practice/grade-9', subject=u'maths',
+    title=u'Maths Grade 9 Intelligent Practice')
 
-# Publish
-wf.doActionFor(products_and_services['maths-grade8-practice'], 'publish')
-wf.doActionFor(products_and_services['maths-grade9-practice'], 'publish')
-
-# Reindex, probably to get the Title right if memory serves
-products_and_services['maths-grade8-practice'].reindexObject()
-products_and_services['maths-grade9-practice'].reindexObject()
+# Create monthly services
+add_service('maths-grade8-monthly-practice',
+    price=15.0, grade=u'grade-8', subscription_period=30,
+    access_path='@@practice/grade-8', subject=u'maths',
+    title=u'Maths Grade 8 Intelligent Practice')
+add_service('maths-grade9-monthly-practice',
+    price=15.0, grade=u'grade-9', subscription_period=30,
+    access_path='@@practice/grade-9', subject=u'maths',
+    title=u'Maths Grade 9 Intelligent Practice')
 
 transaction.commit()
